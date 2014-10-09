c0f62ed02b998bfcba050fd3b47ba0b8a42eb690
Leandro Gargantini <pcfixed20@hotmail.com>
2014-10-09 02:36:17 -0300

    improving findPrimes loop

--- controllers/general.js
+++ controllers/general.js
@@ -1,65 +1,58 @@
 'use strict';
 module.exports = function () {
 
-function findPrimes(number) {
-var primes = [];
-var primesCirc = [0,1,2];
-var sqrtNumber = Math.ceil(Math.sqrt(number));
+	function findPrimes(number) {
+		var primes = [0,1,2];
+		var primesCirc = [0,1,2];
+		var sqrtNumber = Math.ceil(Math.sqrt(number));
 
-//populate array prime
-for (var i = 0; i < number; i++) {
-	primes.push(i);
-}
-//criba de eratostenes
-for(i = 2; i < sqrtNumber; i++){
-	for(var j=i ;(j*i) < number; j++){
-		delete primes[i*j];
-	}
-}
-//circular
-for(i = 3; i < primes.length; i++) {
-	if(!isNaN(primes[i])){
-		if(isCircularPrime(primes[i])){
-			primesCirc.push(primes[i]);
-		}
-	}
-}
-
-return primesCirc;
-}
-
-function isCircularPrime (number) {
-var lengthTopNumber = digits(number);
-var magnification = Math.pow(10, lengthTopNumber - 1 );
-
-	for(var i=0; i < lengthTopNumber; i++){
-			if(!isPrime(number)){
-				return false;
+			//populate array prime
+			for (var i = 3; i < number; i++) {
+				if(isPrime(i)){
+					primes.push(i);
+				}
 			}
-			number = rotatePrime(number,magnification);
+			//circular
+			for(i = 3; i < primes.length; i++) {
+				if(isCircularPrime(primes[i])){
+					primesCirc.push(primes[i]);
+				}
+			}
+			return primesCirc;
 	}
-	return true;
 
-}
-
-function isPrime (val) {
-var sqrtNumber = Math.ceil(Math.sqrt(val));
-	
-	for(var i = 2; i <= sqrtNumber; i++){
-		if(val%i === 0){
+	function isCircularPrime (number) {
+		var lengthTopNumber = digits(number);
+		var magnification = Math.pow(10, lengthTopNumber - 1 );
+	//criba de eratostenes
+	for(var i=0; i < lengthTopNumber; i++){
+		if(!isPrime(number)){
 			return false;
 		}
+		number = rotatePrime(number,magnification);
 	}
 	return true;
-}
 
-function digits (num) {
-	return num.toString().length;
-}
+	}
 
-function rotatePrime(number,magnification) {
-	return Math.floor((number % 10 ) * magnification + (number/10));
-}
+	function isPrime (val) {
+		var sqrtNumber = Math.ceil(Math.sqrt(val));
+		
+		for(var i = 2; i <= sqrtNumber; i++){
+			if(val%i === 0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	function digits (num) {
+		return num.toString().length;
+	}
+
+	function rotatePrime(number,magnification) {
+		return Math.floor((number % 10 ) * magnification + (number/10));
+	}
 
 var gen = {
 	
@@ -72,15 +65,15 @@ var gen = {
 		var number = req.param('number') ? req.param('number') : req.body.numberPost;
 
 		if(req.body.numberPost !== undefined){
-		req.assert('numberPost','Invalid number must be int').isInt();
-		req.assert('numberPost','Must enter a number').notEmpty();
-		req.assert('numberPost','1 to 7 digits allowed').len(1,7);
+			req.assert('numberPost','Invalid number must be int').isInt();
+			req.assert('numberPost','Must enter a number').notEmpty();
+			req.assert('numberPost','1 to 7 digits allowed').len(1,7);
 		}
 
 		if(req.param('number') !== undefined){
-		req.assert('number','Invalid number must be int').isInt();
-		req.assert('number','Must enter a number').notEmpty();
-		req.assert('number','1 to 7 digits allowed').len(1,7);	
+			req.assert('number','Invalid number must be int').isInt();
+			req.assert('number','Must enter a number').notEmpty();
+			req.assert('number','1 to 7 digits allowed').len(1,7);	
 		}
 		
 		//validation
@@ -89,13 +82,13 @@ var gen = {
 		var errors = req.validationErrors();
 		
 		if(errors){
-					res.send({msg: 'There have been validation errors: '+util.inspect(errors)});
+			res.json({msg: 'There have been validation errors: '+util.inspect(errors)});
 		}else{
 
-		var list = findPrimes(Number(number));
-		res.send({msg:'number ok', list:util.inspect(list)});
+			var list = findPrimes(Number(number));
+			res.json({msg:'number ok', list:util.inspect(list)});
 
-		next();
+			next();
 		}
 	},
 
